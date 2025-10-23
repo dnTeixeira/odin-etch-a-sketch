0865622e428197dd5ff53a09e4257569ce7575f2
Daniel Teixeira <danielteixeira33@hotmail.com>
2025-10-23 18:07:36 -0300

    feat: add grid creation logic and styling

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+const container = document.querySelector(".container");
+
+for(let i = 0; i < 256; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add("item-" + (i + 1));
+    newDiv.classList.add("gridItem");
+
+    container.appendChild(newDiv);
+}
