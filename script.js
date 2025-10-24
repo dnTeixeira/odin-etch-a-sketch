6827b62a90ac6729a16a6d1a3cdc036f2d575dd8
Daniel Teixeira <danielteixeira33@hotmail.com>
2025-10-23 21:21:37 -0300

    refactor: wrap existing code into functions

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
 const container = document.querySelector(".container");
 const newGridButton = document.querySelector(".grid-size-button");
+let gridSize = 256;
+let userChoice = 16;
 
-for(let i = 0; i < 256; i++) {
-    const newDiv = document.createElement('div');
-    const divSize = 480 / 16;
-    newDiv.classList.add("item-" + (i + 1));
-    newDiv.classList.add("gridItem");
-
-    newDiv.style.width = divSize + "px";
-    newDiv.style.height = divSize + "px";
-    newDiv.style.border = "1px solid red";
-    newDiv.style.boxSizing = "border-box";
-
-    container.appendChild(newDiv);
+function createGrid() {
+    for(let i = 0; i < gridSize; i++) {
+        const newDiv = document.createElement('div');
+        const divSize = 480 / userChoice;
+        newDiv.classList.add("item-" + (i + 1));
+        newDiv.classList.add("gridItem");
+    
+        newDiv.style.width = divSize + "px";
+        newDiv.style.height = divSize + "px";
+        newDiv.style.border = "1px solid red";
+        newDiv.style.boxSizing = "border-box";
+    
+        container.appendChild(newDiv);
+    }
+    
+    const grid = document.querySelectorAll(".gridItem");
+    
+    grid.forEach(item => 
+        item.addEventListener('mouseover', (e) => {
+            e.target.style.background = "black";
+        })
+    );
 }
 
-const grid = document.querySelectorAll(".gridItem");
+function changeGridSize() {
+    userChoice = parseInt(prompt("Number of squares per side for the new board: (100 is the max)"));
 
-grid.forEach(item => 
-    item.addEventListener('mouseover', (e) => {
-        e.target.style.background = "black";
-    })
-);
+    if(userChoice > 100 || userChoice < 0) {
+       changeGridSize();
+    } else if(!Number.isNaN(userChoice)) {
+        gridSize = userChoice * userChoice;
+        
+        deleteGrid();
+        createGrid();
+    } else {
+        return;
+    }
+
+    console.log(userChoice);
+}
+
+function deleteGrid() {
+    container.querySelectorAll(".gridItem").forEach(item =>
+        item.remove()
+    );
+}
 
 newGridButton.addEventListener('click', (e) => {
-    
+    changeGridSize()
 })
+
+createGrid();
